12a8fa3f1d08e1eaa473a8226ba2519235cb65ac
Jack Fox <jackf19@vt.edu>
2021-04-19 23:58:40 -0400

    created project

---
+++ src/prj5/Input.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author Jack Fox
+ * @version 2021.04.19
+ *
+ */
+public class Input {
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
