462c986ff29294c81fcc1306b3c60832b006d177
gantagiri4 <82853189+gantagiri4@users.noreply.github.com>
2021-04-22 14:16:53 -0400

    Add files via upload

--- src/prj5/LinkedList.java
+++ src/prj5/LinkedList.java
@@ -1,4 +1,4 @@
-package prj5;
+package covidvisualizer;
 
 import list.ListInterface;
 
@@ -8,7 +8,7 @@ public class LinkedList<T> implements ListInterface<T> {
      * This represents a node in a singly linked list. This node stores data
      * along with having a pointer to the next node in the list
      *
-     * @param <D>
+     * @param <T>
      *            This is the type of object that this class will store
      * @author Giri Ganta (gantagiri4)
      * @version 2021.04.19
@@ -189,28 +189,38 @@ public class LinkedList<T> implements ListInterface<T> {
     }
 
 
+    /**
+     * gets the entry at the specified entry within the list.
+     * 
+     * @param index
+     *            the spot within the list that the object is being returned
+     *            from.
+     * 
+     * @return
+     *         the object at the specified entry.
+     */
     @Override
     public T getEntry(int index) {
-        if (isEmpty()) {
-            return null;
-        }
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index is out of bounds");
+            throw new IndexOutOfBoundsException();
         }
-        
-        Node<T> current = head;
-        for (int i = 0; i <= index; i++) {
-            current = current.next();
+        else {
+            Node<T> current = head;
+            int curr = 0;
+            while (curr != index) {
+                current = current.next;
+                curr++;
+            }
+            return current.getData();
         }
-        return current.getData();
-        
     }
 
 
-    @Override
-    public int getLength() {
-        // TODO Auto-generated method stub
-        return 0;
+    /**
+     * Returns the size of the list.
+     */
+    public int getSize() {
+        return size;
     }
 
 
@@ -223,24 +233,193 @@ public class LinkedList<T> implements ListInterface<T> {
     }
 
 
+    /**
+     * Removes an object from the list at the specified index.
+     * 
+     * @param index
+     *            index that the object is being removed from.
+     * 
+     * @return
+     *         the object that was removed.
+     */
     @Override
-    public T remove(int arg0) {
-        // TODO Auto-generated method stub
-        return null;
+    public T remove(int index) {
+        T result = null;
+
+        if (index >= 0 && index < size) {
+            if (index == 0) {
+                result = head.getData();
+                head = head.next;
+            }
+            else {
+                Node<T> current = head;
+                int curr = 0;
+                while (curr + 1 != index) {
+                    current = current.next;
+                    curr++;
+                }
+                result = current.next.getData();
+                current.next = current.next.next;
+            }
+        }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
+        return result;
     }
 
 
+    /**
+     * Replaces an object from the list at the specified index with the
+     * specified entry.
+     * 
+     * @param index
+     *            index that the object is being replaced from.
+     * 
+     * @param anEntry
+     *            object that is replacing the pre-existing entry.
+     * 
+     * @return
+     *         the object that was replaced
+     */
     @Override
-    public T replace(int arg0, T arg1) {
-        // TODO Auto-generated method stub
-        return null;
+    public T replace(int index, T anEntry) {
+        T result = null;
+
+        if (index >= 0 && index < size) {
+            if (index == 0) {
+                result = head.data;
+                head.data = anEntry;
+            }
+            else {
+                Node<T> current = head;
+                int curr = 0;
+                while (curr != index) {
+                    current = current.next;
+                    curr++;
+                }
+                result = current.data;
+                current.data = anEntry;
+            }
+        }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
+        return result;
     }
 
 
+    /**
+     * Returns an array representation of the list If a list contains A, B, and
+     * C, the following should be returned {A, B, C}, If a list
+     * contains A, B, C, and C the following should be returned {A, B, C, C}
+     * 
+     * @return an array representing the list
+     */
     @Override
     public Object[] toArray() {
-        // TODO Auto-generated method stub
-        return null;
+        Object[] array = new Object[size];
+
+        Node<T> current = head;
+        int count = 0;
+        while (current != null) {
+            array[count] = current.getData();
+            current = current.next;
+            count++;
+        }
+
+        return array;
+    }
+
+
+    /**
+     * Returns true if both lists have the exact same contents
+     * in the exact same order
+     *
+     * @return a boolean of whether two lists have the same contents,
+     *         item per item and in the same order
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() == obj.getClass()) {
+            @SuppressWarnings("unchecked")
+            LinkedList<T> other = ((LinkedList<T>)obj);
+            if (other.getSize() == this.getSize()) {
+                Node<T> current = head;
+                Node<T> otherCurrent = other.head;
+                while (current != null) {
+                    if (!current.getData().equals(otherCurrent.getData())) {
+                        return false;
+                    }
+                    current = current.next();
+                    otherCurrent = otherCurrent.next();
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Returns a string representation of the list If a list contains A, B, and
+     * C, the following should be returned "{A, B, C}" (Without the quotations)
+     *
+     * @return a string representing the list
+     */
+    @Override
+    public String toString() {
+        String result = "{";
+
+        Node<T> current = head;
+        while (current != null) {
+            result += "" + current.data;
+            current = current.next;
+            if (current != null) {
+                result += ", ";
+            }
+        }
+        result += "}";
+        return result;
+    }
+
+
+    /**
+     * Gets the last time the given object is in the list
+     *
+     * @param obj
+     *            the object to look for
+     * @return the last position of it. -1 If it is not in the list
+     */
+    public int lastIndexOf(T anEntry) {
+        int lastIndex = -1;
+        Node<T> current = head;
+        int currentIndex = 0;
+        while (current != null) {
+            if (anEntry.equals(current.data)) {
+                lastIndex = currentIndex;
+            }
+            currentIndex++;
+            current = current.next;
+
+        }
+        return lastIndex;
+    }
+
+
+    /**
+     * Gets the length of the list.
+     */
+    @Override
+    public int getLength() {
+        return size;
     }
 
 }
---
+++ src/prj5/State.java
@@ -0,0 +1,84 @@
+package covidvisualizer;
+
+/**
+ * State class that contains the attributes of each state within the
+ * visualization.
+ *
+ * @param <T>
+ *            This is the type of object that this class will store
+ * @author Giri Ganta (gantagiri4)
+ * @version 2021.04.19
+ */
+public class State<T> {
+    // fields
+    private String name;
+    private int totalCases;
+    private int totalDeaths;
+    private LinkedList<T> races;
+
+    /**
+     * Makes a new State Object.
+     * 
+     * @param name
+     *            the name of the state.
+     * @param totalCases
+     *            the total COVID cases in the state.
+     * @param totalDeaths
+     *            the total deaths caused by COVID in the state.
+     */
+    public State(
+        String name,
+        int totalCases,
+        int totalDeaths,
+        LinkedList<T> race) {
+        this.name = name;
+        this.totalCases = totalCases;
+        this.totalDeaths = totalDeaths;
+        race = new LinkedList<T>();
+    }
+
+
+    /**
+     * Getter method to get the name of the state.
+     * 
+     * @return
+     *         the name of the state.
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * Getter method to get the number of total cases for the state.
+     * 
+     * @return
+     *         the number of total cases.
+     */
+    public int getTotalCases() {
+        return totalCases;
+    }
+
+
+    /**
+     * Getter method to get the number of total deaths for the state.
+     * 
+     * @return
+     *         the number of total deaths.
+     */
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+
+    /**
+     * Getter method to get the races of the state.
+     * 
+     * @return
+     *         the races in a linked list.
+     */
+    public LinkedList<T> getRaces() {
+        return races;
+    }
+
+}
