6e166a5d4cbba4f575d525ffa0422ffaa0431b66
gantagiri4 <82853189+gantagiri4@users.noreply.github.com>
2021-04-25 18:32:21 -0400

    Add files via upload

--- src/prj5/StateTest.java
+++ src/prj5/StateTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package prj5;
 
 import student.TestCase;
@@ -13,16 +10,16 @@ import student.TestCase;
  *
  */
 
-//As a Hokie, I will conduct myself with honor and integrity at all times.
-//I will not lie, cheat, or steal, nor will I accept the actions of those who
-//do.
-//-- Jack Fox (jackf19)
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Jack Fox (jackf19)
 
 public class StateTest extends TestCase {
 
     private State s1;
     private LinkedList<Race> llr;
-    
+
     /**
      * Creates scenario for test methods.
      */
@@ -32,65 +29,65 @@ public class StateTest extends TestCase {
         Race r3 = new Race("r3", 23, 2);
         Race r4 = new Race("r4", 11, 1);
         Race r5 = new Race("r5", -1, -1);
-        
+
         llr = new LinkedList<Race>();
         llr.add(r1);
         llr.add(r2);
         llr.add(r3);
         llr.add(r4);
         llr.add(r5);
-        
+
         s1 = new State("s1", llr);
     }
-    
-    
+
+
     /**
      * Tests getName method.
      */
     public void testGetName() {
         assertEquals("s1", s1.getName());
     }
-    
-    
+
+
     /**
      * Tests getTotalCases method.
      */
     public void testGetTotalCases() {
-        assertEquals(84, s1.getTotalCases());
+        assertEquals((double)84, s1.getTotalCases(), 0.01);
     }
-    
-    
+
+
     /**
      * Tests getTotalDeaths method.
      */
     public void testGetTotalDeaths() {
-        assertEquals(23, s1.getTotalDeaths());
+        assertEquals((double)23, s1.getTotalDeaths(), 0.01);
     }
-    
-    
+
+
     /**
      * Tests getRaces method.
      */
     public void testGetRaces() {
         assertEquals(llr, s1.getRaces());
     }
-    
-    
+
+
     /**
      * Tests setTotalCases method.
      */
     public void testSetTotalCases() {
         s1.setTotalCases();
-        assertEquals(84, s1.getTotalCases());
+        assertEquals((double)84, s1.getTotalCases(), 0.01);
     }
-    
-    
+
+
     /**
      * Tests setTotalDeaths method.
      */
     public void testSetTotalDeaths() {
         s1.setTotalDeaths();
-        assertEquals(23, s1.getTotalDeaths());
+        assertEquals((double)23, s1.getTotalDeaths(), 0.01);
     }
-    
+
 }
